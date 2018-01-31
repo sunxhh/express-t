9c722b0c546a59617b6e244b62ae3d2a6b45fb2c
997854244@qq.com <997854244@qq.com>
2018-01-31 11:44:19 +0800

    增加body-parser类型

--- middlewares/body-parser/index.js
+++ middlewares/body-parser/index.js
@@ -1,4 +1,4 @@
-var parsePostBody = function(req, done) {
+var parsePostBody = function (req, done) {
     var arr = [];
     var chunks;
 
@@ -8,11 +8,13 @@ var parsePostBody = function(req, done) {
 
     req.on('end', () => {
         chunks = Buffer.concat(arr);
-
         done(chunks);
     });
 };
-module.exports = function(req, res, next) {
+
+var parsers = Object.create(null);
+
+module.exports = function (req, res, next) {
     parsePostBody(req, (chunks) => {
         var body = chunks.toString();
         body = body.split("\r\n");
@@ -20,3 +22,36 @@ module.exports = function(req, res, next) {
         next();
     });
 };
+
+
+
+function createParserGetter(name) {
+    return function get() {
+        return loadParser(name)
+    }
+}
+
+
+
+function loadParser(parserName) {
+    var parser = parsers[parserName]
+
+    if (parser !== undefined) {
+        return parser
+    }
+    switch (parserName) {
+        case 'json':
+            parser = require('./lib/types/json')
+            break
+        case 'raw':
+            parser = require('./lib/types/raw')
+            break
+        case 'text':
+            parser = require('./lib/types/text')
+            break
+        case 'urlencoded':
+            parser = require('./lib/types/urlencoded')
+            break
+    }
+    return (parsers[parserName] = parser)
+}

e2df8e4f23f76b599ba5c92021c510fa95a8a5ff
sxh <997854244@qq.com>
2018-02-11 18:10:23 +0800

    增加urlencode

--- middlewares/body-parser/types/urlencoded.js
+++ middlewares/body-parser/types/urlencoded.js
@@ -5,18 +5,38 @@ let parseUrlencode = function (str) {
     let param = {};
     paramList.forEach(ele => {
         let e = ele.split("=");
-        param[decodeURIComponent(e[0])] = decodeURIComponent(e[1]);
+        if (e[0]) {
+            param[decodeURIComponent(e[0])] = decodeURIComponent(e[1]);
+        }
     });
     return param;
 }
 
+function urlSearch(req) {
+    let json = {};
+    let name, value;
+    let str = req.url; //取得整个地址栏
+    let num = str.indexOf("?");
+    str = str.substr(num + 1); //取得所有参数
+    let arr = str.split("&"); //各个参数放到数组里
+    for (let i = 0; i < arr.length; i++) {
+        num = arr[i].indexOf("=");
+        if (num > 0) {
+            name = arr[i].substring(0, num);
+            value = arr[i].substr(num + 1);
+            json[decodeURIComponent(name)] = decodeURIComponent(value);
+        }
+    }
+    return json;
+}
 let handleParam = function (req, chunks) {
+    let urlParam = urlSearch(req);
     let param = chunks.toString();
-    _body = parseUrlencode(param);
-    console.log(_body);
+    let _body = parseUrlencode(param);
+    _body = Object.assign(urlParam, _body);
     req.body = _body;
+    console.log(_body);
 };
-
 
 
 module.exports = function (req, res, next) {
